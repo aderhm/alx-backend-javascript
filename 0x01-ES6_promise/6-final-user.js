3e767d85b98eead0a324a18387eabcd0f274a9fa
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-06-26 22:42:38 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => console.log(values))
-    .catch(() => {});
+  const output = [];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => {
+      data.forEach((d) => {
+        if (d.status === 'rejected') {
+          output.push({ status: d.status, value: String(d.reason) });
+        } else {
+          output.push(d);
+        }
+      });
+      return output;
+    });
 }
