dc609b82acc01dc04d6a8cc38b8e44b8705560ff
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-06-26 19:05:02 +0100

    Resolve/Reject a promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,13 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ stauts: 200, body: 'Success' });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
+  }).then((res) => {
+    console.log(res);
+  }).catch((err) => {
+    console.error(err);
   });
 }
