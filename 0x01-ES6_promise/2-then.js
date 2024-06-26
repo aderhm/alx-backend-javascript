8ec2394bcc22477489c15fca2ea3f06293d27646
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-06-26 19:33:39 +0100

    Catch

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  // eslint-disable-next-line no-unused-vars
-  return promise.then((res) => {
-    console.log({ status: 200, body: 'success' });
-  })
-    // eslint-disable-next-line no-unused-vars
-    .catch((err) => {
-      console.error(new Error(''));
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }))
+    .catch(() => new Error(''))
+    .finally(() => console.log('Got a response from the API'));
 }
