d058869d642ad364d307554bbed5e9d583ccadaa
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-06-27 19:31:21 +0100

    Create a class Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('cuurency must be an Currency');
+    } else {
+      this._currency = value;
+    }
+  }
+
+  displayFullPrice() {
+    const fullCurrency = this._currency.displayFullCurrency();
+    return `${this._amount} ${fullCurrency}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
