2f17d6f7b0997e003f471d1e2158c4b4d30dbd5d
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-07-01 22:16:31 +0100

    Combine objects

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,10 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   const studentsFromCity = arr.filter((obj) => obj.location === city);
-  studentsFromCity.forEach((element) => {
-    const elementGrades = newGrades.filter((gr) => gr.studentId === element.id);
-    if (elementGrades[0]) {
-      // eslint-disable-next-line no-param-reassign
-      element.grade = elementGrades[0].grade;
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      element.grade = 'N/A';
-    }
+  return studentsFromCity.map((obj) => {
+    const stdGrade = newGrades.filter((gr) => gr.studentId === obj.id)[0];
+    return ({
+      ...obj,
+      grade: stdGrade ? stdGrade.grade : 'N/A',
+    });
   });
-  return studentsFromCity;
 }
